53410357a6b2da978e38c99991d15b4c54fb0ace
Chris Lowles <lowleschris@gmail.com>
2026-01-28 03:16:36 +1100

    Update cleanup.sh

--- config/scripts/cleanup.sh
+++ config/scripts/cleanup.sh
@@ -2,11 +2,9 @@
 
 echo "Cleaning up build artifacts..."
 
-# Remove temporary files
 rm -rf /tmp/config
 rm -f /tmp/*.sh
 
-# Clean package manager cache
 rpm-ostree cleanup -m
 
 echo "Cleanup complete."
