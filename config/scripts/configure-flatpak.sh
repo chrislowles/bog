441202f84aaaa9c63e69bc69aab808b3d4d1779f
Chris Lowles <lowleschris@gmail.com>
2026-01-28 01:48:36 +1100

    Update configure-flatpak.sh

--- config/scripts/configure-flatpak.sh
+++ config/scripts/configure-flatpak.sh
@@ -2,20 +2,7 @@
 
 echo "Configuring Flatpak..."
 
-# Flatpak should already be installed in the base image, but ensure it's present
-# Don't fail if it's already installed
-set +e
-rpm-ostree install flatpak 2>/dev/null
-flatpak_result=$?
-set -e
-
-if [ $flatpak_result -eq 0 ]; then
-    echo "Flatpak installed successfully"
-else
-    echo "Flatpak already present or installation not needed"
-fi
-
-# Flathub remote will be added at runtime by the system
+# Flatpak is already installed in the base image
 # We can't add remotes or install flatpaks during the image build
 # Users will add flathub and install flatpaks after booting
 
