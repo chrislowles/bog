15955c5fc348252d75ef00360c2361743993f5f4
Chris Lowles <lowleschris@gmail.com>
2026-01-28 03:16:37 +1100

    Update configure-flatpak.sh

--- config/scripts/configure-flatpak.sh
+++ config/scripts/configure-flatpak.sh
@@ -2,9 +2,5 @@
 
 echo "Configuring Flatpak..."
 
-# Flatpak is already installed in the base image
-# We can't add remotes or install flatpaks during the image build
-# Users will add flathub and install flatpaks after booting
-
 echo "Flatpak configuration complete."
 echo "Note: Flathub remote and flatpaks will be configured at first boot or by user"
