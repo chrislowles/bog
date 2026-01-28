8eebf7ac088165d791b7c0995cf144601a8da075
Chris Lowles <lowleschris@gmail.com>
2026-01-29 06:21:32 +1100

    Update configure-flatpak.sh

--- config/scripts/configure-flatpak.sh
+++ config/scripts/configure-flatpak.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
+
 echo "Configuring Flatpak with Flathub..."
+
 # Add Flathub repository system-wide
-flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists --system flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 # Enable Flathub by default
 flatpak remote-modify --enable flathub
+
 echo "Flatpak configuration complete."
+
 echo "Flathub is now available at first boot."
