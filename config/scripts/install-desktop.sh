518acf39c32ef40acb9f7ac19d8dd9634421d663
Chris Lowles <lowleschris@gmail.com>
2026-01-29 07:47:07 +1100

    Update install-desktop.sh

--- config/scripts/install-desktop.sh
+++ config/scripts/install-desktop.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-rpm-ostree install \
-	NetworkManager-wifi NetworkManager-wwan NetworkManager-tui \
-	gnome-shell gdm gnome-session gnome-settings-daemon gnome-keyring xdg-desktop-portal-gnome xdg-user-dirs-gtk \
-	gnome-terminal gnome-control-center gnome-system-monitor nautilus gnome-text-editor gnome-disk-utility gnome-logs baobab dconf-editor \
-	gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad-free \
-	google-noto-sans-fonts google-noto-serif-fonts google-noto-emoji-fonts liberation-fonts
+echo "Installing crucial system components"
+rpm-ostree install NetworkManager-wifi NetworkManager-wwan NetworkManager-tui
+
+echo "Installing GNOME/GDM root system applications"
+rpm-ostree install gnome-shell gdm gnome-session gnome-settings-daemon gnome-keyring xdg-desktop-portal-gnome xdg-user-dirs-gtk gnome-terminal gnome-control-center gnome-system-monitor nautilus gnome-text-editor gnome-disk-utility gnome-logs baobab dconf-editor
+
+echo "Installing gstreamer plugins"
+rpm-ostree install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad-free
+
+echo "Installing fonts"
+rpm-ostree install google-noto-sans-fonts google-noto-serif-fonts google-noto-emoji-fonts liberation-fonts
