987dabeb2efc181ab47ddfb392f89cf9a6171057
Chris Lowles <lowleschris@gmail.com>
2026-02-24 23:00:57 +1100

    Update aliases.sh

--- files/system/etc/profile.d/aliases.sh
+++ files/system/etc/profile.d/aliases.sh
@@ -1,3 +1,17 @@
+alias pls="sudo"
+
+distrobox_arch_create() {
+    distrobox create --pull -Y -n arch -i archlinux:latest -ap "base-devel git" -- bash -c "distrobox_arch_enter;distrobox_arch_yay;echo 'You can now install AUR packages!"
+}
+
+distrobox_arch_enter() {
+    distrobox enter arch
+}
+
+distrobox_arch_yay() {
+    git clone https://aur.archlinux.org/yay-bin.git -- bash -c "cd yay-bin;makepkg -si"
+}
+
 getmp4() {
     yt-dlp --format "bestvideo+bestaudio/best" --embed-subs --embed-thumbnail --embed-metadata --embed-chapters "$@"
 }
@@ -7,8 +21,8 @@ getmp3() {
 }
 
 set_nextdns() {
-    read -rp "Enter IPv4 addresses (space-separated): " ipv4_1 ipv4_2
-    read -rp "Enter IPv6 addresses (space-separated): " ipv6_1 ipv6_2
+    read -rp "Enter NextDNS-provided IPv4 addresses (space-separated): " ipv4_1 ipv4_2
+    read -rp "Enter NextDNS-provided IPv6 addresses (space-separated): " ipv6_1 ipv6_2
 
     local con
     con="$(nmcli -t -f NAME con show --active | head -1)"
@@ -22,29 +36,29 @@ set_nextdns() {
         && echo "DNS updated successfully on: $con"
 }
 
-bazflatcpr() {
+bfp_cpr() {
 
-    ### UNFINISHED (EMERGENCY REBOOT FUNCTION FOR FLATPAK AND BAZAAR)
+    ### UNFINISHED EMERGENCY REBOOT FUNCTION FOR FLATPAK AND BAZAAR
 
     echo "=== FLATPAK/BAZAAR CPR ==="
     sleep 2
 
     echo "FLATPAK/BAZAAR CPR: Attempting to kill and revive processes..."
-    sudo pkill -9 -f flatpak
-    sudo pkill -9 -f bazaar
-    sudo pkill -9 -f flatpak-system-helper
+    pls pkill -9 -f flatpak
+    pls pkill -9 -f bazaar
+    pls pkill -9 -f flatpak-system-helper
 
     echo "FLATPAK/BAZAAR CPR: rm -f /var/lib/flatpak/.changed"
-    sudo rm -f /var/lib/flatpak/.changed
+    pls rm -f /var/lib/flatpak/.changed
 
-    sudo mkdir -p /etc/bazzite
+    pls mkdir -p /etc/bazzite
 
-    echo "9" | sudo tee /etc/bazzite/flatpak_manager_version
+    echo "9" | pls tee /etc/bazzite/flatpak_manager_version
     mount | grep revokefs
 
     echo "FLATPAK/BAZAAR CPR: Killed flatpak and Bazaar processes"
 
-    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    pls flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     echo "FLATPAK/BAZAAR CPR: Reimported Flathub repo"
 
     flatpak repair
