1a8fb4c7920702a71ebb907eecdab3486d3d63b0
Chris Lowles <lowleschris@gmail.com>
2026-03-01 07:20:05 +1100

    Update aliases.sh

--- files/system/etc/profile.d/aliases.sh
+++ files/system/etc/profile.d/aliases.sh
@@ -13,16 +13,16 @@ distrobox_arch_yay() {
 }
 
 getmp4() {
-    yt-dlp --format "bestvideo+bestaudio/best" --embed-subs --embed-thumbnail --embed-metadata --embed-chapters "$@"
+    yt-dlp --format "bestvideo+bestaudio/best" --embed-metadata --embed-thumbnail --embed-subs --embed-chapters "$@"
 }
 
 getmp3() {
-    yt-dlp --format "bestaudio" --extract-audio --audio-format "mp3" --audio-quality "0" --embed-thumbnail --embed-metadata "$@"
+    yt-dlp --format "bestaudio" --embed-metadata --embed-thumbnail --extract-audio --audio-format "mp3" --audio-quality "0" "$@"
 }
 
 set_nextdns() {
-    read -rp "Enter NextDNS-provided IPv4 addresses (space-separated): " ipv4_1 ipv4_2
-    read -rp "Enter NextDNS-provided IPv6 addresses (space-separated): " ipv6_1 ipv6_2
+    read -rp "Enter the 2 NextDNS-provided IPv4 addresses (space-separated): " ipv4_1 ipv4_2
+    read -rp "Enter the 2 NextDNS-provided IPv6 addresses (space-separated): " ipv6_1 ipv6_2
 
     local con
     con="$(nmcli -t -f NAME con show --active | head -1)"
@@ -34,46 +34,4 @@ set_nextdns() {
         ipv6.ignore-auto-dns yes \
         && nmcli con up "$con" \
         && echo "DNS updated successfully on: $con"
-}
-
-bfp_cpr() {
-
-    ### UNFINISHED EMERGENCY REBOOT FUNCTION FOR FLATPAK AND BAZAAR
-
-    echo "=== FLATPAK/BAZAAR CPR ==="
-    sleep 2
-
-    echo "FLATPAK/BAZAAR CPR: Attempting to kill and revive processes..."
-    pls pkill -9 -f flatpak
-    pls pkill -9 -f bazaar
-    pls pkill -9 -f flatpak-system-helper
-
-    echo "FLATPAK/BAZAAR CPR: rm -f /var/lib/flatpak/.changed"
-    pls rm -f /var/lib/flatpak/.changed
-
-    pls mkdir -p /etc/bazzite
-
-    echo "9" | pls tee /etc/bazzite/flatpak_manager_version
-    mount | grep revokefs
-
-    echo "FLATPAK/BAZAAR CPR: Killed flatpak and Bazaar processes"
-
-    pls flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    echo "FLATPAK/BAZAAR CPR: Reimported Flathub repo"
-
-    flatpak repair
-    echo "FLATPAK/BAZAAR CPR: Attempted to repair flatpak apps"
-
-    flatpak update --appstream
-    echo "FLATPAK/BAZAAR CPR: Updated flatpak apps (appstream)"
-
-    # Refresh application launcher cache (currently kde plasma, looking to change to refreshing the gnome menu cache if needed)
-    if command -v kbuildsycoca6 &> /dev/null; then
-        echo "FLATPAK/BAZAAR CPR: Refreshing application launcher cache."
-        kbuildsycoca6 &>/dev/null
-    elif command -v kbuildsycoca5 &> /dev/null; then
-        echo "FLATPAK/BAZAAR CPR: Refreshing application launcher cache."
-        kbuildsycoca5 &>/dev/null
-    fi
-
 }
