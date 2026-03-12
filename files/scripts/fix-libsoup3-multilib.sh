ace779881c88ac39a8fbed3a1f2d227ae649ce53
Chris Lowles <lowleschris@gmail.com>
2026-03-12 22:12:33 +1100

    Delete fix-libsoup3-multilib.sh

--- files/scripts/fix-libsoup3-multilib.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Force libsoup3 x86_64 to 3.6.6 directly from Koji to match i686 in updates, resolving the multilib version mismatch that blocks the main dnf transaction.
-# Will be removed in the future.
-dnf install -y \
-    https://kojipkgs.fedoraproject.org/packages/libsoup3/3.6.6/1.fc43/x86_64/libsoup3-3.6.6-1.fc43.x86_64.rpm \
-    https://kojipkgs.fedoraproject.org/packages/libsoup3/3.6.6/1.fc43/i686/libsoup3-3.6.6-1.fc43.i686.rpm
