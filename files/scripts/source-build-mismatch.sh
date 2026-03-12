490b56a6ab56026e3dc907832a346f53e21f0785
Chris Lowles <lowleschris@gmail.com>
2026-03-12 22:32:38 +1100

    Update source-build-mismatch.sh

--- files/scripts/source-build-mismatch.sh
+++ files/scripts/source-build-mismatch.sh
@@ -2,8 +2,3 @@
 set -oue pipefail
 
 # For if/when there's dependency mismatches/lags for source-built programs
-
-# Force libsoup3 x86_64 to 3.6.6 directly from Koji to match i686 in updates, resolving the multilib version mismatch that blocks the main dnf transaction when dealing with the source build of Bazaar (will be removed in the future)
-#dnf install -y \
-#    https://kojipkgs.fedoraproject.org/packages/libsoup3/3.6.6/1.fc43/x86_64/libsoup3-3.6.6-1.fc43.x86_64.rpm \
-#    https://kojipkgs.fedoraproject.org/packages/libsoup3/3.6.6/1.fc43/i686/libsoup3-3.6.6-1.fc43.i686.rpm
