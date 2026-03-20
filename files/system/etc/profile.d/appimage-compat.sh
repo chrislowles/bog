109dd4b7a14f1e8aabefe28df9adc0340e090685
Chris Lowles <lowleschris@gmail.com>
2026-03-20 17:22:10 +1100

    Update appimage-compat.sh

--- files/system/etc/profile.d/appimage-compat.sh
+++ files/system/etc/profile.d/appimage-compat.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Straightening out variables for AppImages so they are more reliable.
+
 export HOME=/var/home/$USER
 export APPIMAGE_EXTRACT_AND_RUN=1
 export TMPDIR=~/.cache/appimage-tmp
