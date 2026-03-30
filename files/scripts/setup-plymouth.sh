18ad4dec5527e9f782d532a2b3c7ff66a37995ad
Chris Lowles <lowleschris@gmail.com>
2026-03-31 03:26:43 +1100

    Delete setup-plymouth.sh

--- files/scripts/setup-plymouth.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-if [ ! -f /usr/share/plymouth/themes/bog/bog.plymouth ]; then
-    echo "ERROR: bog theme not found"
-    exit 1
-fi
-
-plymouth-set-default-theme bog
