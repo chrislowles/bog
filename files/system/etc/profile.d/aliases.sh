f79906b0547c18330272ce75a487682dbebe82b0
Chris Lowles <lowleschris@gmail.com>
2026-04-24 17:38:47 +1000

    Update aliases.sh

--- files/system/etc/profile.d/aliases.sh
+++ files/system/etc/profile.d/aliases.sh
@@ -64,20 +64,78 @@ getmedia() {
     esac
 }
 
-# TODO: refactor "set_nextdns" as generic "setdns" function,
-# nextdns as a provider under "--provider nextdns" and flags like "-ipv6-1 <1st-ipv6-addr> -ipv6-2 <2nd-ipv6-addr> -ipv4-1 <1st-ipv4-addr> -ipv4-2 <2nd-ipv2-addr>" to expediate the setting being set,
-# none written prompts the process to add the addresses as directed below.
-set_nextdns() {
+# setdns --provider <name> [-ipv6-1 <addr>] [-ipv6-2 <addr>] [-ipv4-1 <addr>] [-ipv4-2 <addr>]
+#
+# Configures DNS for the active NetworkManager connection.
+# Supported providers: nextdns (more can be added)
+#
+# Addresses can be supplied inline via flags, or left out to be prompted interactively.
+#
+# Examples:
+#   setdns --provider nextdns
+#   setdns --provider nextdns -ipv6-1 2a07::1 -ipv6-2 2a07::2 -ipv4-1 45.90.28.0 -ipv4-2 45.90.30.0
+setdns() {
+    local provider="" ipv6_1="" ipv6_2="" ipv4_1="" ipv4_2=""
+
+    # Parse arguments
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --provider) provider="${2:-}"; shift 2 ;;
+            -ipv6-1)    ipv6_1="${2:-}";   shift 2 ;;
+            -ipv6-2)    ipv6_2="${2:-}";   shift 2 ;;
+            -ipv4-1)    ipv4_1="${2:-}";   shift 2 ;;
+            -ipv4-2)    ipv4_2="${2:-}";   shift 2 ;;
+            *)
+                echo "Unknown option: $1"
+                echo "Usage: setdns --provider <name> [-ipv6-1 <addr>] [-ipv6-2 <addr>] [-ipv4-1 <addr>] [-ipv4-2 <addr>]"
+                echo "Supported providers: nextdns"
+                return 1
+                ;;
+        esac
+    done
+
+    if [[ -z "$provider" ]]; then
+        echo "Usage: setdns --provider <name> [-ipv6-1 <addr>] [-ipv6-2 <addr>] [-ipv4-1 <addr>] [-ipv4-2 <addr>]"
+        echo "Supported providers: nextdns"
+        return 1
+    fi
+
+    case "$provider" in
+        nextdns)
+            echo "Configuring NextDNS..."
+            # Prompt for any addresses not supplied via flags
+            [[ -z "$ipv6_1" ]] && read -rp "Enter your 1st NextDNS IPv6 address: " ipv6_1
+            [[ -z "$ipv6_2" ]] && read -rp "Enter your 2nd NextDNS IPv6 address: " ipv6_2
+            [[ -z "$ipv4_1" ]] && read -rp "Enter your 1st NextDNS IPv4 address: " ipv4_1
+            [[ -z "$ipv4_2" ]] && read -rp "Enter your 2nd NextDNS IPv4 address: " ipv4_2
+            ;;
+        *)
+            echo "Unknown provider: $provider"
+            echo "Supported providers: nextdns"
+            return 1
+            ;;
+    esac
+
+    # Validate that all four addresses were provided
+    if [[ -z "$ipv6_1" || -z "$ipv6_2" || -z "$ipv4_1" || -z "$ipv4_2" ]]; then
+        echo "Error: all four DNS addresses are required."
+        return 1
+    fi
+
     local connection
     connection="$(nmcli -t -f NAME connection show --active | head -1)"
 
-    read -rp "Enter your two NextDNS-provided IPv6 addresses (space-separated): " ipv6_1 ipv6_2
-    read -rp "Enter your two NextDNS-provided IPv4 addresses (space-separated): " ipv4_1 ipv4_2
+    if [[ -z "$connection" ]]; then
+        echo "Error: no active NetworkManager connection found."
+        return 1
+    fi
 
+    echo "Applying DNS to connection: $connection"
     nmcli connection modify "$connection" \
         ipv6.dns "$ipv6_1 $ipv6_2" ipv6.ignore-auto-dns yes \
         ipv4.dns "$ipv4_1 $ipv4_2" ipv4.ignore-auto-dns yes \
-        && nmcli connection up "$connection"
+        && nmcli connection up "$connection" \
+        && echo "Done. DNS set to $provider on '$connection'."
 }
 
 # This is simply me automating a part of the process of clearing cache for apps
