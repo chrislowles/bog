17fc2b2a1b10d7a0fe32d44bae4d23282008d1b1
Chris Lowles <lowleschris@gmail.com>
2026-04-29 18:03:40 +1000

    Update source-build-mismatch.sh

--- files/scripts/source-build-mismatch.sh
+++ files/scripts/source-build-mismatch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# For if/when there's dependency mismatches/lags for source-built programs
+echo "source-build-mismatch.sh: waiting in the wings for if/when there's dependency mismatches/lags for source-built programs"
