b6e89be2d8cdc81f973eb97bbda5b66732f9a18f
Chris Lowles <lowleschris@gmail.com>
2026-08-08 17:03:27 +1000

    Update appimage-compat.sh

--- files/system/etc/profile.d/appimage-compat.sh
+++ files/system/etc/profile.d/appimage-compat.sh
@@ -3,7 +3,9 @@
 # Straightening out variables for AppImages so they are more reliably openable.
 
 export HOME=/var/home/$USER
-export APPIMAGE_EXTRACT_AND_RUN=1
+
 export TMPDIR=~/.cache/appimage-tmp
-export ELECTRON_OZONE_PLATFORM_HINT=auto
 mkdir -p "$TMPDIR"
+
+export APPIMAGE_EXTRACT_AND_RUN=1
+export ELECTRON_OZONE_PLATFORM_HINT=auto
