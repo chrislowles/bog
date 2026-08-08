7ba48671bcaec30c018aaeb952650b655d2abb56
Chris Lowles <lowleschris@gmail.com>
2026-08-08 17:03:22 +1000

    Update setup-os-release.sh

--- files/scripts/setup-os-release.sh
+++ files/scripts/setup-os-release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# TODO: restore old versioning in actual os-release variables, I'd just like the name in the bootloader screen to just read as: bog <fedora-base-version> (<shortform git version tag for image, idk how to source this here lol>)
+
 source /usr/lib/os-release
 FEDORA_VERSION="${VERSION_ID}"
 BUILD_DATE=$(date +%Y%m%d)
