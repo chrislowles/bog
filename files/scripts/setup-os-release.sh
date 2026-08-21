3060a7472a31864c808e1607d8eb8bced992bd91
Chris Lowles <lowleschris@gmail.com>
2026-08-21 23:01:53 +1000

    Update setup-os-release.sh

--- files/scripts/setup-os-release.sh
+++ files/scripts/setup-os-release.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 source /usr/lib/os-release
-sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"bog ${VERSION_ID}\"|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"bog ${OSTREE_VERSION}\"|" /usr/lib/os-release
 
-echo "os-release PRETTY_NAME stamped: bog ${VERSION_ID}"
+echo "os-release PRETTY_NAME stamped: bog ${OSTREE_VERSION}"
