cacae556ca89d0a9a08993aa4ac854d09ad8c8f2
Chris Lowles <lowleschris@gmail.com>
2026-09-14 03:27:55 +1000

    Delete setup-os-release.sh

--- files/scripts/setup-os-release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-source /usr/lib/os-release
-sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"bog ${OSTREE_VERSION}\"|" /usr/lib/os-release
-
-echo "os-release PRETTY_NAME stamped: bog ${OSTREE_VERSION}"
